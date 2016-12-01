5aecead7a16037f0102dd643a28a693f11f5aaef
Jose Manuel Bermudo <jmbermudo@Joses-MacBook-Air.local>
2016-12-01 17:02:52 +0100

    Added unit test for services

--- src/app/shell/shell.component.spec.ts
+++ src/app/shell/shell.component.spec.ts
@@ -1,37 +1,47 @@
 import { ComponentFixture, TestBed, inject } from '@angular/core/testing';
 import { By }              from '@angular/platform-browser';
 import { DebugElement }    from '@angular/core';
+import { MaterialModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 // Load the implementations that should be tested
 import { ShellComponent } from './shell.component';
 
-let comp:    ShellComponent;
-let fixture: ComponentFixture<ShellComponent>;
-let de:      DebugElement;
-let el:      HTMLElement;
+let component:  ShellComponent;
+let fixture:    ComponentFixture<ShellComponent>;
+let debug:      DebugElement;
+let element:    HTMLElement;
+const routes = [{ path: '', component: ShellComponent}];
 
 describe('App Shell', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEach(() => {
      TestBed.configureTestingModule({
-      declarations: [ ShellComponent ], // declare the test component
+      imports: [
+        RouterModule.forRoot(routes),
+        MaterialModule.forRoot()
+      ],
+      declarations: [ 
+        ShellComponent 
+      ], // declare the test component
+      providers: [{provide: APP_BASE_HREF, useValue : '/' }]
     })
     .compileComponents(); // compile template and css
 
     fixture = TestBed.createComponent(ShellComponent);
 
-    comp = fixture.componentInstance; // ShellComponent test instance
+    component = fixture.componentInstance; // ShellComponent test instance
 
-    // query for the title <h1> by CSS element selector
-    de = fixture.debugElement.query(By.css('a'));
-    console.log(de);
-    el = de.nativeElement;
+    // query for the first <a> by CSS element selector
+    debug = fixture.debugElement.query(By.css('a'));
+
+    element = debug.nativeElement;
   });
 
-  it('should have a url',() => {
+  it('should have first route called Home',() => {
     fixture.detectChanges();
-    expect(el.textContent).toContain('home');
-
+    expect(element.textContent).toContain('Home');
   });
 
 });
