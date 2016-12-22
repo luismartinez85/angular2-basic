0b09096884b4ab2cdab658d2e8a750caa03cfb00
Unknown <luispoker85@gmail.com>
2016-12-22 11:56:11 +0100

    update webpack.test.js

--- src/app/modules/+home/components/home/home.component.spec.ts
+++ src/app/modules/+home/components/home/home.component.spec.ts
@@ -2,7 +2,7 @@ import {
   inject,
   TestBed
 } from '@angular/core/testing';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import {
   BaseRequestOptions,
   ConnectionBackend,
@@ -12,30 +12,37 @@ import { MockBackend } from '@angular/http/testing';
 import { FeaturesService } from '../../services/features/features.service';
 import { TechsService } from '../../services/techs/techs.service';
 import { MultiLanguageService } from '../../../../shared/services/multiLanguage.service'; 
+import { EnvironmentConfigService } from '../../../../shared/services/environmentConfig.service'; 
+
+
+import { MaterialModule } from '@angular/material';
+import { TranslateModule } from 'ng2-translate';
 
 // Load the implementations that should be tested
 import { HomeComponent } from './home.component';
 
 describe('Home', () => {
-  let component;
+  let component: HomeComponent;
   // provide our implementations or mocks to the dependency injector
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [
-        BaseRequestOptions,
-        HomeComponent,
-        FeaturesService,
-        TechsService,
-        MultiLanguageService
-      ]
-    });
+  //  declarations:[FsCustomGridCardsComponent],
+      imports: [
+        TranslateModule.forRoot(),
+        MaterialModule.forRoot()
+      ],
+      providers: [EnvironmentConfigService],      
+      declarations:[HomeComponent],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
+    })
+    .compileComponents(); // compile template and css
     const fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
   });
-/*
+
   it('should have a defined component', () => {
     expect(component).toBeDefined();
-  });*/
+  });
   // it('should have a rowHeight', inject([ HomeComponent ], (home: HomeComponent) => {
   //   expect(!!home.rowHeight).toEqual(true);
   // }));
