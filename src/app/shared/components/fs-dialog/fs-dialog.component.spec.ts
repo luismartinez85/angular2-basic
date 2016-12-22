a9fc64c594010cac6f7d4489266cd5406d0683fc
Jose Manuel Bermudo <josemanuelbd@gmail.com>
2016-12-22 16:53:13 +0100

    fsDialog

--- src/app/shared/components/fs-dialog/fs-dialog.component.spec.ts
+++ src/app/shared/components/fs-dialog/fs-dialog.component.spec.ts
@@ -1,20 +1,25 @@
-// import {
-//   inject,
-//   TestBed
-// } from '@angular/core/testing';
-//
-// // Load the implementations that should be tested
-// import { CoreComponent } from './core.component';
-//
-// describe('App', () => {
-//   // provide our implementations or mocks to the dependency injector
-//   beforeEach(() => TestBed.configureTestingModule({
-//     providers: [
-//       CoreComponent
-//     ]}));
-//
-//   it('should have a url', inject([ CoreComponent ], (app: CoreComponent) => {
-//     expect(app.url).toEqual('https://twitter.com/AngularClass');
-//   }));
-//
-// });
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FsDialogComponent } from './fs-dialog.component';
+
+
+let component:  FsDialogComponent;
+let fixture:    ComponentFixture<FsDialogComponent>;
+
+describe('FsDialog Component', () => {
+   beforeEach(() => {
+    // refine the test module by declaring the test component
+    TestBed.configureTestingModule({
+      declarations: [ FsDialogComponent ],
+    }).compileComponents();
+
+    // create component and test fixture
+    fixture = TestBed.createComponent(FsDialogComponent);
+
+    // get test component from the fixture
+    component = fixture.componentInstance;
+  });
+
+  it('should be defined', () => {
+     expect(component).toBeDefined();
+  });
+})
