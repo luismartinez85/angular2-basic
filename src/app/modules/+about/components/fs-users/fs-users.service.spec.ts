7f7e53634147813c4336b061fd30be2870c1443b
Jose Manuel Bermudo <josemanuelbd@gmail.com>
2016-12-27 16:26:56 +0100

    implementing fsUsers unit test

--- src/app/modules/+about/components/fs-users/fs-users.service.spec.ts
+++ src/app/modules/+about/components/fs-users/fs-users.service.spec.ts
@@ -1,22 +1,59 @@
-/*
-import {BaseRequestOptions, Http} from '@angular/http';
-import {MockBackend} from '@angular/http/testing';
+import { async, inject, TestBed } from '@angular/core/testing';
+import { BaseRequestOptions, Http, HttpModule, Response, ResponseOptions } from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
-it('should get some data', inject([AsyncTestCompleter], (async) => {
-  var connection;
-  var injector = Injector.resolveAndCreate([
-    MockBackend,
-    {provide: Http, useFactory: (backend, options) => {
-      return new Http(backend, options);
-    }, deps: [MockBackend, BaseRequestOptions]}]);
-  var http = injector.get(Http);
-  var backend = injector.get(MockBackend);
-  //Assign any newly-created connection to local variable
-  backend.connections.subscribe(c => connection = c);
-  http.request('data.json').subscribe((res) => {
-    expect(res.text()).toBe('awesome');
-    async.done();
+import { FsUsersService } from './fs-users.service';
+
+let usersJSONMock = {
+  "users" : [{
+      "name": "David Chavarri",
+      "job": "Front Architect",
+      "image": "/assets/img/team/david-min.jpg",
+      "description": "React Ninja",
+      "github": "https://github.com/DvdChavarri"
+    }]
+};
+
+describe('Modules -> About -> FsUsersService', () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        FsUsersService,
+
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend, options) => new Http(backend, options),
+          deps: [MockBackend, BaseRequestOptions]
+        }
+      ],
+      imports: [
+        HttpModule
+      ]
+    });
   });
-  connection.mockRespond(new Response('awesome'));
-}));
-*/
+
+  it('should be defined', async(inject(
+    [FsUsersService, MockBackend], (service, mockBackend) => {
+      expect(service).toBeDefined();
+  })));
+
+  it('should search and return users mock', async(inject(
+    [FsUsersService, MockBackend], (service, mockBackend) => {
+
+    mockBackend.connections.subscribe(conn => {
+      conn.mockRespond(new Response(new ResponseOptions({ body: usersJSONMock })));
+    });
+
+    const result = service.search();
+
+    result.subscribe(res => {
+      expect(res).toEqual(usersJSONMock);
+    });
+  })));
+
+
+
+});
