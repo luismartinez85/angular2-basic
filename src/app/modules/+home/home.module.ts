b223cc418fa670f872294135eecbd51cf9764d23
Jose Manuel Bermudo <josemanuelbd@gmail.com>
2016-12-28 17:59:37 +0100

    refactoring home

--- src/app/modules/+home/home.module.ts
+++ src/app/modules/+home/home.module.ts
@@ -10,6 +10,7 @@ import { HomeMainComponent } from './components/home-main/home-main.component';
 import { HomeTechnologiesComponent } from './components/home-technologies/home-technologies.component';
 import { HomeFeaturesComponent } from './components/home-features/home-features.component';
 import { HomeFeatureModal } from './components/home-feature-modal/home-feature-modal.component';
+import { HomeLogoComponent } from './components/home-logo/home-logo.component';
 
 import { routes } from './home.routes';
 import  { ShellModule } from '../../shell';
@@ -23,14 +24,14 @@ import { MaterialModule } from '@angular/material';
     ExponentialStrengthPipe,
     HomeTechnologiesComponent,
     HomeFeaturesComponent,
-    HomeFeatureModal
+    HomeFeatureModal,
+    HomeLogoComponent
   ],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
     MaterialModule.forRoot(),
     TranslateModule.forRoot()
-
   ],
   entryComponents : [
     HomeFeatureModal
