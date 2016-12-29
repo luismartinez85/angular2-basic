83a3289937722de4f6d254f5dc97d7805684fdff
Luis Martinez <luispoker85@gmail.com>
2016-12-29 11:59:34 +0100

    update routes as constants

--- src/app/shared/components/toolbar/toolbar.component.ts
+++ src/app/shared/components/toolbar/toolbar.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-
+import { ROUTES_CONSTANTS } from '../../constants/routes/routes.constants';
 /*
  * Toolbar Component
  */
 @Component({
   selector: 'toolbar',
   styleUrls: ['toolbar.component.scss'],
-  templateUrl: 'toolbar.component.html',
+  templateUrl: 'toolbar.component.html'
 })
 
-export class ToolbarComponent { }
+export class ToolbarComponent {
+  ROUTES: any
+  constructor(){
+    this.ROUTES = ROUTES_CONSTANTS;
+  }
+
+ }
