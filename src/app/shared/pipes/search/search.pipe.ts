df5aaf44fadd1a5aeeba85eef0afac49610ef7c9
Luis Martinez <luispoker85@gmail.com>
2017-01-09 17:03:18 +0100

    update search pipe

--- src/app/shared/pipes/search/search.pipe.ts
+++ src/app/shared/pipes/search/search.pipe.ts
@@ -5,17 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
   pure: false
 })
 export class SearchPipe implements PipeTransform {
-  transform(data: any[], searchTerm: string, key: string): any[] {
-    if (!data || searchTerm === ''){
+  transform(data: any[], searchText: string, key: string): any[] {
+    if (!data || searchText === ''){
       return data;
     }
 
-    searchTerm = searchTerm.toLowerCase();
+    searchText = searchText.toLowerCase();
     return data.filter(item => {
       if (!item[key]){
         return;
       }
-      return item[key].toLowerCase().indexOf(searchTerm) !== -1;
+      return item[key].toLowerCase().indexOf(searchText) !== -1;
     });
   }
 }
