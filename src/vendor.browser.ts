8a6052f69c486b53e30e4121f4ccdfac8a6832ab
Luis Martinez <luispoker85@gmail.com>
2017-01-19 12:00:48 +0100

    update webpack bundle files size

--- src/vendor.browser.ts
+++ src/vendor.browser.ts
@@ -6,17 +6,25 @@
 // TODO(gdi2290): switch to DLLs
 
 // Angular 2
-import '@angular/platform-browser';
-import '@angular/platform-browser-dynamic';
+
 import '@angular/core';
 import '@angular/common';
 import '@angular/forms';
 import '@angular/http';
+import '@angular/platform-browser';
+import '@angular/platform-browser-dynamic';
 import '@angular/router';
 
 // RxJS
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/Rx';
+import 'hammerjs';
+
+// angular Material
+import '@angular/material';
+// ng2-translate
+import 'ng2-translate';
 
 if ('production' === ENV) {
   // Production
