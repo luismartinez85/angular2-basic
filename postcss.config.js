12f1d9531bf21c5f4ac9dcb6fce6c8bceee270fa
Jose Manuel Bermudo <josemanuelbd@gmail.com>
2017-01-23 15:25:10 +0100

    fixed postcss

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-smart-import')({ /* ...options */ }),
     require('precss')({ /* ...options */ }),
-    require('autoprefixer')({ /* ...options */ }),
     require('postcss-cssnext')(),
     require('postcss-apply')(),
     require('postcss-responsive-type')()
