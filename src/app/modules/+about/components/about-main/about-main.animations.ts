9ce33344eaa6466e7dcbcee6219eba98b3fff60f
Luis Martinez <luispoker85@gmail.com>
2017-02-06 12:23:48 +0100

    fix lint

--- src/app/modules/+about/components/about-main/about-main.animations.ts
+++ src/app/modules/+about/components/about-main/about-main.animations.ts
@@ -1,15 +1,15 @@
-import {trigger, state, animate, style, transition} from '@angular/core';
+import { trigger, state, animate, style, transition } from '@angular/core';
 
 export function routerTransition() {
   return trigger('routerTransition', [
     transition('void => *', [
-        // when the component enters
+      // when the component enters
       style({opacity: 0}),
       animate(500, style({opacity: 1}))
     ]),
     transition('* => void', [
-        // when the component leaves
-        animate(500, style({opacity: 0}))
+      // when the component leaves
+      animate(500, style({opacity: 0}))
     ])]
   );
 }
--- src/app/modules/+home/components/home-main/home-main.animations.ts
+++ src/app/modules/+home/components/home-main/home-main.animations.ts
@@ -3,12 +3,12 @@ import { trigger, state, animate, style, transition } from '@angular/core';
 export function routerTransition() {
   return trigger('routerTransition', [
     transition('void => *', [
-        // when the component enters
+      // when the component enters
       style({opacity: 0}),
       animate(500, style({opacity: 1}))
     ]),
     transition('* => void', [
-        // when the component leaves
+      // when the component leaves
       animate(500, style({opacity: 0}))
     ])]
   );
