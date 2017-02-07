0d848b4e63c0b2cd7cadbd32f132d0a16a2ed354
Luis Martinez <luispoker85@gmail.com>
2017-02-07 12:46:04 +0100

    update file loader

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -164,7 +164,7 @@ module.exports = function (options) {
          */
         {
           test: /\.(jpg|png|gif)$/,
-          loader: 'file-loader?name=assets/img/[folder]/[name].[ext]'
+          loader: 'file-loader?name=./assets/img/[folder]/[name].[ext]'
         }
       ]
 
